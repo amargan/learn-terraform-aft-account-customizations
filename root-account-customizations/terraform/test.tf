c8eb251ab0f99a7224c9337895c036cdd96561a6
Aleks Margan <aleks.margan@philips.com>
2024-12-07 12:36:37 -0500

    add assume role for aws cli

--- root-account-customizations/terraform/test.tf
+++ root-account-customizations/terraform/test.tf
@@ -13,12 +13,35 @@ output "caller_user" {
 }
 
 
-resource "null_resource" "test" {
-  # depends_on = [aws_organizations_policy_attachment.sandbox_whitelist_attachment_1,
-  # aws_organizations_policy_attachment.sandbox_whitelist_attachment_2]
+# resource "null_resource" "test" {
+#   depends_on = [aws_organizations_policy_attachment.sandbox_whitelist_attachment_1,
+#   aws_organizations_policy_attachment.sandbox_whitelist_attachment_2]
 
+#   provisioner "local-exec" {
+#     command = "aws organizations detach-policy --policy-id p-FullAWSAccess --target-id ${aws_organizations_organizational_unit.sandbox_whitelist.id}"
+#   }
+# }
+
+
+resource "null_resource" "assume_role_command" {
   provisioner "local-exec" {
-    #    command = "aws organizations detach-policy --policy-id p-FullAWSAccess --target-id ${aws_organizations_organizational_unit.sandbox_whitelist.id}
-    command = "aws sts get-caller-identity"
+    command = <<EOF
+      credentials=($(aws sts assume-role \
+        --role-arn "arn:aws:iam::195094525803:role/AWSAFTExecution" \
+        --role-session-name "terraform-session-null-exec" \
+        --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+        --output text))
+
+      unset PROFILE
+      export AWS_ACCESS_KEY_ID="${credentials[0]}"
+      export AWS_SECRET_ACCESS_KEY="${credentials[1]}"
+      export AWS_SESSION_TOKEN="${credentials[2]}"
+      unset credentials
+      
+      aws sts get-caller-identity
+    EOF
+
+    interpreter = ["/bin/bash", "-c"]
   }
 }
+
