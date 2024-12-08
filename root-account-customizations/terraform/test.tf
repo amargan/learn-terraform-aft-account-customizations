0423781e696ee69843e70a7782831d89c21d0c96
Aleks Margan <aleks.margan@philips.com>
2024-12-08 08:53:54 -0500

    full use case

--- root-account-customizations/terraform/test.tf
+++ root-account-customizations/terraform/test.tf
@@ -14,35 +14,35 @@ output "caller_user" {
 
 #############################
 
-# resource "aws_organizations_organizational_unit" "sandbox_whitelist" {
-#   name = "Sandbox-Whitelist"
-#   #  parent_id = data.aws_ssm_parameter.sandbox_ou.value
-#   parent_id = "r-xg04"
-# }
+resource "aws_organizations_organizational_unit" "sandbox_whitelist" {
+  name = "Sandbox-Whitelist"
+  #  parent_id = data.aws_ssm_parameter.sandbox_ou.value
+  parent_id = "r-xg04"
+}
 
 
-# resource "aws_organizations_policy" "sandbox_whitelist_1" {
-#   name        = "sandbox-whitelist-1"
-#   description = "Sandbox Whitelist SCP1"
-#   content     = file("policies/scp/sandbox-whitelist-ou-scp1.json")
-# }
+resource "aws_organizations_policy" "sandbox_whitelist_1" {
+  name        = "sandbox-whitelist-1"
+  description = "Sandbox Whitelist SCP1"
+  content     = file("policies/scp/sandbox-whitelist-ou-scp1.json")
+}
 
-# resource "aws_organizations_policy_attachment" "sandbox_whitelist_attachment_1" {
-#   policy_id = aws_organizations_policy.sandbox_whitelist_1.id
-#   target_id = aws_organizations_organizational_unit.sandbox_whitelist.id
-# }
+resource "aws_organizations_policy_attachment" "sandbox_whitelist_attachment_1" {
+  policy_id = aws_organizations_policy.sandbox_whitelist_1.id
+  target_id = aws_organizations_organizational_unit.sandbox_whitelist.id
+}
 
 
-# resource "aws_organizations_policy" "sandbox_whitelist_2" {
-#   name        = "sandbox-whitelist-2"
-#   description = "Sandbox Whitelist SCP2"
-#   content     = file("policies/scp/sandbox-whitelist-ou-scp2.json")
-# }
+resource "aws_organizations_policy" "sandbox_whitelist_2" {
+  name        = "sandbox-whitelist-2"
+  description = "Sandbox Whitelist SCP2"
+  content     = file("policies/scp/sandbox-whitelist-ou-scp2.json")
+}
 
-# resource "aws_organizations_policy_attachment" "sandbox_whitelist_attachment_2" {
-#   policy_id = aws_organizations_policy.sandbox_whitelist_2.id
-#   target_id = aws_organizations_organizational_unit.sandbox_whitelist.id
-# }
+resource "aws_organizations_policy_attachment" "sandbox_whitelist_attachment_2" {
+  policy_id = aws_organizations_policy.sandbox_whitelist_2.id
+  target_id = aws_organizations_organizational_unit.sandbox_whitelist.id
+}
 
 
 
@@ -56,13 +56,31 @@ output "caller_user" {
 # }
 
 
-resource "null_resource" "example1" {
+resource "null_resource" "example2" {
+  depends_on = [
+    aws_organizations_policy_attachment.sandbox_whitelist_attachment_1,
+    aws_organizations_policy_attachment.sandbox_whitelist_attachment_2
+  ]
+
   provisioner "local-exec" {
     environment = {
-      AWS_PROFILE = "aft-target"
+      AWS_PROFILE  = "aft-target"
+      target_ou_id = aws_organizations_organizational_unit.sandbox_whitelist.id
     }
 
     interpreter = ["/bin/bash", "-c"]
-    command     = "aws sts get-caller-identity"
+    command     = "aws organizations detach-policy --policy-id p-FullAWSAccess --target-id $target_ou_id"
   }
 }
+
+
+# resource "null_resource" "example1" {
+#   provisioner "local-exec" {
+#     environment = {
+#       AWS_PROFILE = "aft-target"
+#     }
+
+#     interpreter = ["/bin/bash", "-c"]
+#     command     = "aws sts get-caller-identity"
+#   }
+# }
